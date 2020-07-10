851615fde4fd1252f70cefc9be2018fe16521d70
dingyang <mrding0815@163.com>
2020-07-10 18:32:20 +0800

    完成登录注册页的布局

---
+++ components/LoginForm.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="loginContainer">
+    <el-input
+      placeholder="请输入用户名"
+      v-model="username"
+      class="input username"
+      clearable>
+    </el-input>
+    <el-input
+      placeholder="请输入密码"
+      v-model="password"
+      class="input password"
+      clearable
+      show-password>
+    </el-input>
+    <div class="forgetPwd">
+      <a href="javascript:void(0)">忘记密码</a>
+    </div>
+    <el-button type="primary" class="login">登录</el-button>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "LoginForm",
+    data() {
+      return {
+        username: '',
+        password:''
+      }
+    }
+  }
+</script>
+
+<style lang="less" scoped>
+  .loginContainer{
+    display: flex;
+    justify-content: center;
+    flex-wrap: wrap;
+    padding:0 24px 24px 24px;
+    .input{
+      margin-top: 24px;
+    }
+    .forgetPwd{
+      width: 100%;
+      font-size: 12px;
+      margin: 24px 0 10px 0;
+      display: flex;
+      color: #40aaff;
+      justify-content: flex-end;
+    }
+    .login{
+      width: 100%;
+    }
+  }
+
+</style>
---
+++ components/RegisterForm.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="registerContainer">
+    <el-input
+      placeholder="请输入手机号"
+      v-model="phoneNum"
+      class="input phoneNum"
+      clearable>
+    </el-input>
+    <el-input placeholder="请输入验证码" v-model="authCode" class="input-with-select input">
+      <el-button slot="append">获取验证码</el-button>
+    </el-input>
+    <el-input
+      placeholder="请输入昵称"
+      v-model="nickname"
+      class="input nickname"
+      clearable>
+    </el-input>
+    <el-input
+      placeholder="输入密码"
+      v-model="password"
+      class="input password"
+      show-password
+      clearable>
+    </el-input>
+    <el-input
+      placeholder="再次确认密码"
+      v-model="newPassword"
+      class="input newPassword"
+      show-password
+      clearable>
+    </el-input>
+    <el-button type="primary" class="register">马上注册</el-button>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "RegisterForm",
+    data() {
+      return {
+        phoneNum: '',
+        authCode:'',
+        nickname:'',
+        password:'',
+        newPassword:''
+      }
+    },
+  }
+</script>
+
+<style lang="less" scoped>
+  .registerContainer {
+    display: flex;
+    justify-content: center;
+    flex-wrap: wrap;
+    padding: 0 24px 24px 24px;
+
+    .input {
+      margin-top: 24px;
+    }
+    .register{
+      width: 100%;
+      margin-top: 24px;
+    }
+  }
+</style>
