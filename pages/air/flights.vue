b439904673973c06ec5cfeed3034a47b0302c050
MrDing-web <mrding0815@163.com>
2020-07-17 19:30:54 +0800

    完成了过滤机票的选项设置

--- pages/air/flights.vue
+++ pages/air/flights.vue
@@ -4,7 +4,41 @@
       <!-- 顶部过滤列表 -->
       <div class="flights-content">
         <!-- 过滤条件 -->
-        <div></div>
+        <div>
+          <span>单程：</span><span>{{$route.query.departCity}}</span> - <span>北京</span> / <span>2020-07-17 </span>
+          <el-select v-model="airVal" placeholder="起飞机场" size="mini" class = "selectInput">
+            <el-option
+              v-for="item in airportOption"
+              :key="item.value"
+              :value="item.value"
+            >
+            </el-option>
+          </el-select>
+          <el-select v-model="timeVal" placeholder="起飞时间" size="mini" class = "selectInput">
+            <el-option
+              v-for="item in departTimeOption"
+              :key="item.value"
+              :label="item.label"
+              :value="item.value">
+            </el-option>
+
+          </el-select>
+          <el-select v-model="comVal" placeholder="航空公司" size="mini" class = "selectInput">
+            <el-option
+              v-for="item in comOption"
+              :key="item.value"
+              :value="item.value">
+            </el-option>
+          </el-select>
+          <el-select v-model="planeVal" placeholder="机型大小" size="mini" class = "selectInput">
+            <el-option
+              v-for="item in planeOption"
+              :key="item.value"
+              :label="item.label"
+              :value="item.value">
+            </el-option>
+          </el-select>
+        </div>
 
         <!-- 航班头部布局 -->
         <FlightsListHead />
@@ -49,26 +83,88 @@ export default {
     return {
       flightsList: "",
       pageSize:5,
-      currentPage: 1
+      currentPage: 1,
+      airportOption:[],
+      departTimeOption:[
+        {
+          label:"0:00 - 4:00",
+          value:"0,4"
+        },
+        {
+          label:"4:00 - 8:00",
+          value:"4,8"
+        },
+        {
+          label:"8:00 - 12:00",
+          value:"8,12"
+        },
+        {
+          label:"12:00 - 16:00",
+          value:"12,16"
+        },
+        {
+          label:"16:00 - 20:00",
+          value:"16,20"
+        },
+        {
+          label:"20:00 - 24:00",
+          value:"20,24"
+        }
+      ],
+      comOption:[],
+      planeOption:[
+        {
+          label:"小",
+          value:"S"
+        },
+        {
+          label:"中",
+          value:"M"
+        },
+        {
+          label:"大",
+          value:"L"
+        }
+      ],
+      airVal:"",
+      timeVal:"",
+      comVal:"",
+      planeVal:""
     };
   },
-  created() {
-    // this.$store.dispatch("/user/login");
-  },
+  created() {},
   mounted() {
     this.$axios({
       url: "/airs",
       params: this.$route.query,
     }).then(res => {
       this.flightsList = res.data.flights;
+      console.log(this.flightsList);
+      if(this.flightsList === '') return;
+      this.getFiltersOption();
     });
+  },
+  watch:{
+
+  },
+  filters:{
+
   },
   methods: {
     handleSizeChange(val) {
       this.pageSize = val;
+
+      // flightsList | filterA | filterB | filterC | filterD
+
+
     },
     handleCurrentChange(val) {
       this.currentPage = val;
+    },
+    getFiltersOption(){
+      //加载过滤器选项
+      this.airportOption = [...new Set(this.flightsList.map(item =>item.dst_airport_name))].map(i=>({value:i}));
+      this.comOption = [...new Set(this.flightsList.map(item =>item.airline_name))].map(j=>({value:j}))
     }
   },
   computed:{
@@ -88,6 +184,11 @@ export default {
 .flights-content {
   width: 745px;
   font-size: 14px;
+}
+
+.selectInput{
+  width: 120px;
+  margin: 10px 0 10px 10px;
 }
 
 .aside {
